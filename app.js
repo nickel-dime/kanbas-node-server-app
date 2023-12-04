32f6a6933c6facc433c11626326e2d1f651a2c08
nickel-dime <goepnik@gmail.com>
2023-12-04 13:21:50 -0500

    Add user routes and model

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
@@ -6,12 +11,29 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
+UserRoutes(app);
 
 Hello(app);
 app.listen(process.env.PORT || 4000);
